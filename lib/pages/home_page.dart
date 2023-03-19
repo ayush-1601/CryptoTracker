d8986a8fc4cd37c52da8b0ad0364a3fed60fdf94
ayush-1601 <bansalayush1601@gmail.com>
2023-03-19 15:35:07 +0530

    added tab views, and favourite page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_crypto_tracker/models/Crypto_currency.dart';
+import 'package:flutter_crypto_tracker/pages/Market_list.dart';
 import 'package:flutter_crypto_tracker/pages/details_page.dart';
+import 'package:flutter_crypto_tracker/pages/favourites_page.dart';
 import 'package:flutter_crypto_tracker/providers/market_provider.dart';
 import 'package:flutter_crypto_tracker/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -15,7 +17,16 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController viewController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    viewController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     ThemeProvider themeProvider =
@@ -48,88 +59,26 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 20,
             ),
+            TabBar(controller: viewController, tabs: [
+              Tab(
+                child: Text(
+                  "Markets",
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Favourites",
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              )
+            ]),
             Expanded(
-              child: Consumer<MarketProvider>(
-                  builder: (context, marketProvider, child) {
-                if (marketProvider.isLoading == true) {
-                  return Center(
-                      child: CircularProgressIndicator(
-                    color: Colors.amber,
-                  ));
-                } else {
-                  if (marketProvider.markets.isNotEmpty) {
-                    return RefreshIndicator(
-                      onRefresh: () async {
-                        await marketProvider.fetchData();
-                      },
-                      child: ListView.builder(
-                        physics: BouncingScrollPhysics(
-                            parent: AlwaysScrollableScrollPhysics()),
-                        itemCount: marketProvider.markets.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          CruptoCurrency currentCrypto =
-                              marketProvider.markets[index];
-                          return ListTile(
-                            onTap: (() {
-                              Navigator.push(
-                                  context,
-                                  CupertinoPageRoute(
-                                      builder: (context) => DetailsPage(
-                                            id: currentCrypto.id!,
-                                          )));
-                            }),
-                            // shape: RoundedRectangleBorder(
-                            //     borderRadius: BorderRadius.circular(10)),
-                            // tileColor: Color.fromARGB(255, 39, 41, 50),
-                            contentPadding: EdgeInsets.all(0),
-                            leading: CircleAvatar(
-                              backgroundColor: Colors.white,
-                              backgroundImage:
-                                  NetworkImage(currentCrypto.image!),
-                            ),
-                            title: Text(currentCrypto.name! +
-                                " #${currentCrypto.marketCapRank!}"),
-                            subtitle: Text(currentCrypto.symbol!.toUpperCase()),
-                            trailing: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Text(
-                                  "INR ${currentCrypto.currentPrice!.toStringAsFixed(4)}",
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      color: Color.fromARGB(255, 20, 99, 163),
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Builder(builder: (context) {
-                                  double priceChange =
-                                      currentCrypto.priceChange24H!;
-                                  double priceChangePercentage =
-                                      currentCrypto.priceChangePercentage24H!;
-
-                                  if (priceChange < 0) {
-                                    return Text(
-                                      "${priceChangePercentage.toStringAsFixed(2)}% (${priceChange.toStringAsFixed(4)})",
-                                      style: TextStyle(color: Colors.red),
-                                    );
-                                  } else {
-                                    return Text(
-                                      "+${priceChangePercentage.toStringAsFixed(2)}% (+${priceChange.toStringAsFixed(4)})",
-                                      style: TextStyle(color: Colors.green),
-                                    );
-                                  }
-                                })
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  } else {
-                    return Text("data not found");
-                  }
-                }
-              }),
+              child: TabBarView(
+                  physics: BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics()),
+                  controller: viewController,
+                  children: [MarketList(), Favourites()]),
             )
           ],
         ),
